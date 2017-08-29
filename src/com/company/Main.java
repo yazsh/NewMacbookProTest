0c323cf87324c1f26fbabea22d7cca79122c2e93
Yazen2017 <yazen.shunnar@gmail.com>
2017-08-29 13:49:44 -0400

    First Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import sun.security.ssl.Debug;
+
+public class Main {
+
+    public static void  main(String[] args) {
+
+        Debug.println("printing");
+    }
+
+
+
+
+}
